bec8539f24d04f6ab5afdf68b2500f0be985a8ae
Abhay Kumar <abhay.kumar@cordis.us>
2023-01-10 13:58:01 +0530

    updated

--- AbhayKumar/src/app/employee-details/employee-details.component.ts
+++ AbhayKumar/src/app/employee-details/employee-details.component.ts
@@ -11,14 +11,13 @@ export class EmployeeDetailsComponent implements OnInit {
   constructor(private fb:FormBuilder) { }
 
   isChanged:boolean=false;
+  isChanges : boolean[]= [];
   ngOnInit(): void {
   }
 
-
-
   employeeForm = this.fb.group({
     employeeDetails: this.fb.array([]),// Initially,  the FormArray instance is empty and contains no form controls, meaning that the editable table is initially empty.
-}); 
+  }); 
 
 deafultImageSource="../../assets/images/Sachin.jpg";
 fileToUpload: File | null = null;
@@ -38,10 +37,13 @@ fileToUpload: File | null = null;
       userGender : new FormControl("" , [Validators.required]),
     })
     empArr.push(empDetails);  // or this.employeeDetails.push(empDetails)
+    this.isChanges[empArr.length-1] = false;
+    console.log("isChanges :- " , this.isChanges)
   }
 
   deleteEmployee(lessonIndex: number) {
     this.employeeDetails.removeAt(lessonIndex);
+    this.isChanges.splice(lessonIndex,1);
   }
 
 
@@ -52,29 +54,40 @@ fileToUpload: File | null = null;
     console.log("employeeForm.value.employeeDetails[1].userImage.value :-  ",employeeForm.value.employeeDetails[0].userImage.value)
   } 
 
-
+  // accessShowPrievew(event: any , index:any){
+  //   this.showPreview(event , index)
+  // }
 
   showPreview(event: any , index:any){
-    this.isChanged =false;
+    // this.isChanged =false;
+    this.isChanges.splice(index,1,false);
+    // this.isChanges.splice(lessonIndex,1);
     console.log("event  :-  ",event);
     console.log("event.target.files  :-  ",event.target.files);
     let files = event.target.files
     this.fileToUpload = files.item(0);
     if(files){
-      this.isChanged =true;
+      // this.isChanged =true;
+      this.isChanges.splice(index,1,true);
       let reader = new FileReader();  
       reader.readAsDataURL(files[0]);
       reader.onload=(event:any)=>{
           let imgResult= event.target.result;
           let runInterval = 0;
           const myInterval= setInterval(()=>{
-            this.isChanged = false;
+            // this.isChanged = false;
+            this.isChanges.splice(index,1,false);
             runInterval +=1;
             console.log("isChanged  :-  ",this.isChanged);
             if(runInterval === 1){
               clearInterval(myInterval);
             }
           },3000);
+          // const myInterval= setTimeout(()=>{
+          //   this.isChanged = false;
+          // this.isChanges.splice(index,1,false);
+          //   console.log("isChanged  :-  ",this.isChanged);
+          // },3000);
           this.employeeForm.controls.employeeDetails.controls[index].patchValue({userImage:imgResult});
       }
     }
